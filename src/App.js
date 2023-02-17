6c23c1a3d15c906e420ebccb4306783e73831be2
Mika Bibas <mikabibas@gmail.com>
2023-02-17 17:30:27 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [subtotal, setSubtotal] = useState(0);
+  const [numDiners, setNumDiners] = useState(0);
+  const [tipPercentage, setTipPercetnage] = useState(0);
+  const [result, setResult] = useState({});
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (+subtotal <= 0 || +numDiners <= 0 || +tipPercentage <= 0) {
+      return;
+    }
+    const total = +subtotal * (1 + +tipPercentage);
+    setResult({ total, totalPerDiner: +total / +numDiners });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <fieldset className="subtotal">
+          <label>subtotal</label>
+          <input
+            value={subtotal}
+            onChange={(e) => setSubtotal(e.target.value)}
+          />
+        </fieldset>
+        <fieldset className="diners">
+          <label>number of people sharing the bill</label>
+          <input
+            value={numDiners}
+            onChange={(e) => setNumDiners(e.target.value)}
+          />
+        </fieldset>
+        <fieldset className="percentage">
+          <label>tip percentage</label>
+          <select
+            className="selectList"
+            value={tipPercentage}
+            onChange={(e) => setTipPercetnage(e.target.value)}
+          >
+            <option value="0">0%</option>
+            <option value="0.05">5%</option>
+            <option value="0.1">10%</option>
+            <option value="0.15">15%</option>
+            <option value="0.2">20%</option>
+          </select>
+        </fieldset>
+        <button type="submit">Calculate</button>
+      </form>
+      <p>total: {result.total && result.total.toFixed(2)}</p>
+      <p>
+        total per diner:{" "}
+        {result.totalPerDiner && result.totalPerDiner.toFixed(2)}
+      </p>
     </div>
   );
 }
-
-export default App;
